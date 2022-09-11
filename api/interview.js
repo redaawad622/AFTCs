0a532276676ba0dd69b51d9572a647738fd725f8
Reda Awad <redaawad622@gmail.com>
2022-09-11 11:28:47 +0200

    Fixed interview form checking bug

--- api/interview.js
+++ api/interview.js
@@ -20,54 +20,19 @@ module.exports = function (app, prisma) {
   })
 
   app.post('/saveInterview', async (req, res) => {
-    const {
-      id,
-      national_id,
-      marital_state,
-      educational_degree,
-      parent_job,
-      siblings_num,
-      family_relation,
-      complaint,
-      appearance,
-      focus_ability,
-      mood,
-      speaking_disorder,
-      medicine_type,
-      has_medical_history,
-      hospital_name,
-      drugs_history,
-      drug_type,
-      final_opinion,
-      examiner_status,
-      final_hospital_result,
-      order_brothers,
-      rel_between_parents,
-      personal_medical,
-      family_medical,
-      family_income,
-      half_brothers,
-      parent_rel,
-      complaint_f,
-      transReason,
-      moving,
-      faceExprission,
-      timeAware,
-      situationAware,
-      judgeAbility,
-      awareDisorder,
-      thinkDisorder,
-      appetite,
-      sleeping,
-      smoking,
-      prayer,
-      interviewer_opinion,
-      recommendation,
-      recommendation_res,
-      recommendation_summary,
-      interviewer,
-      historyDate,
-    } = req.body
+    const { id, national_id, marital_state, educational_degree, historyDate } =
+      req.body
+
+    const interviewData = { ...req.body }
+    for (const key in interviewData) {
+      if (
+        Object.hasOwnProperty.call(interviewData, key) &&
+        !interviewData[key]
+      ) {
+        delete interviewData[key]
+      }
+    }
+    interviewData.historyDate = new Date(historyDate)
     const examinerData = await prisma.Examiners.update({
       where: {
         national_id,
@@ -77,99 +42,25 @@ module.exports = function (app, prisma) {
         educational_degree,
       },
     })
+    interviewData.examiner_id = examinerData.id
+    delete interviewData.marital_state
+    delete interviewData.educational_degree
+    delete interviewData.id
+    delete interviewData.national_id
+    delete interviewData.name
+    delete interviewData.stage
+    delete interviewData.qualification_code
+    delete interviewData.sold_id
+    delete interviewData.examiner_id
+
     const interview = await prisma.Interview.upsert({
       where: {
         id: Number(id),
       },
-      create: {
-        parent_job,
-        siblings_num,
-        family_relation,
-        complaint,
-        appearance,
-        focus_ability,
-        mood,
-        speaking_disorder,
-        medicine_type,
-        half_brothers,
-        parent_rel,
-        has_medical_history,
-        hospital_name,
-        drugs_history,
-        drug_type,
-        final_opinion,
-        examiner_status,
-        final_hospital_result,
-        examiner_id: examinerData.id,
-        order_brothers,
-        rel_between_parents,
-        personal_medical,
-        family_medical,
-        family_income,
-        complaint_f,
-        transReason,
-        moving,
-        faceExprission,
-        timeAware,
-        situationAware,
-        judgeAbility,
-        awareDisorder,
-        thinkDisorder,
-        appetite,
-        sleeping,
-        smoking,
-        prayer,
-        interviewer_opinion,
-        recommendation,
-        recommendation_res,
-        recommendation_summary,
-        interviewer,
-        historyDate: new Date(historyDate),
-      },
+      create: interviewData,
       update: {
-        parent_job,
-        siblings_num,
-        family_relation,
-        complaint,
-        appearance,
-        focus_ability,
-        mood,
-        speaking_disorder,
-        half_brothers,
-        parent_rel,
-        medicine_type,
-        has_medical_history,
-        hospital_name,
-        drugs_history,
-        drug_type,
-        final_opinion,
-        examiner_status,
-        final_hospital_result,
-        order_brothers,
-        rel_between_parents,
-        personal_medical,
-        family_medical,
-        family_income,
+        ...interviewData,
         updated_at: new Date(),
-        complaint_f,
-        transReason,
-        moving,
-        faceExprission,
-        timeAware,
-        situationAware,
-        judgeAbility,
-        awareDisorder,
-        thinkDisorder,
-        appetite,
-        sleeping,
-        smoking,
-        prayer,
-        interviewer_opinion,
-        recommendation,
-        recommendation_res,
-        recommendation_summary,
-        interviewer,
-        historyDate: new Date(historyDate),
       },
     })
     res.json(interview)
